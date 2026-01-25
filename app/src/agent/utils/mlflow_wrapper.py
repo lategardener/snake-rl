61809856073bffec49a2c3fcb6e85012bfca0ec4
lategardener <djolemarc@cy-tech.fr>
2026-01-25 22:04:06 +0100

    feat: add MLflow wrapper for loading and predicting with Hugging Face models

---
+++ app/src/agent/utils/mlflow_wrapper.py
@@ -0,0 +1,35 @@
+import mlflow.pyfunc
+class SnakeHFModel(mlflow.pyfunc.PythonModel):
+    """
+    Classe Wrapper qui permet à MLflow de savoir comment charger et utiliser
+    ton modèle stocké sur Hugging Face.
+    """
+    def __init__(self, repo_id: str, subfolder: str):
+        # On sauvegarde les infos pour retrouver le modèle plus tard
+        self.repo_id = repo_id
+        self.subfolder = subfolder
+        self.model = None
+
+    def load_context(self, context):
+        # Cette méthode est exécutée quand on charge le modèle via mlflow.pyfunc.load_model()
+        from huggingface_hub import hf_hub_download
+        from stable_baselines3 import PPO
+
+        print(f"📥 Chargement du contexte modèle depuis {self.repo_id}...")
+
+        # Téléchargement du fichier physique
+        model_path = hf_hub_download(
+            repo_id=self.repo_id,
+            filename=f"{self.subfolder}/model.zip"
+        )
+
+        # Chargement en mémoire
+        self.model = PPO.load(model_path)
+
+    def predict(self, context, model_input, params=None):
+        """
+        MLflow exige que le paramètre s'appelle 'model_input'.
+        Ici, model_input correspondra à ton observation.
+        """
+        action, _ = self.model.predict(model_input)
+        return action
